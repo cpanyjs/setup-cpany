6ccaaf36830664d1a8d44208c2b57156f3550a7b
XLor <yjl9903@vip.qq.com>
2021-11-14 03:27:27 +0800

    feat: check cpany.yml

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,44 @@
-import * as core from '@actions/core'
+import { join } from 'path';
+import { existsSync, readFileSync } from 'fs';
 
-async function run(): Promise<void> {
-  core.info('Run setup-cpany')
+import * as core from '@actions/core';
+import { load } from 'js-yaml';
+
+interface ICPanyConfig {
+  plugins: string[];
 }
 
-run()
+function getRoot(): string {
+  const root = core.getInput('root');
+  if (root) {
+    return join(process.cwd(), root);
+  } else {
+    return process.cwd();
+  }
+}
+
+function loadCPanyConfig(root: string): ICPanyConfig {
+  const configPath = join(root, 'cpany.yml');
+  if (!existsSync(configPath)) {
+    core.error(`${configPath} is not found`);
+    process.exit(1);
+  }
+  core.startGroup('cpany.yml');
+  const content = readFileSync(configPath, 'utf-8');
+  core.info(content);
+  core.endGroup();
+  try {
+    return load(content) as ICPanyConfig;
+  } catch (error) {
+    core.error(`${error}`);
+    process.exit(1);
+  }
+}
+
+async function run(): Promise<void> {
+  const root = getRoot();
+  core.info(`Root: ${root}`);
+  loadCPanyConfig(root);
+}
+
+run();
